ecf099b46205e5e9d154e6f4e60741ff057091f1
Eisuke15 <hideyu41691@gmail.com>
2022-06-11 00:48:37 +0900

    initialize bias parameters

--- main.py
+++ main.py
@@ -142,6 +142,7 @@ def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0)
     elif classname.find('BatchNorm') != -1:
         torch.nn.init.normal_(m.weight, 1.0, 0.02)
         torch.nn.init.zeros_(m.bias)
